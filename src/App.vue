068b9a5c53a19b0916d40e6f93f74578229122d9
Joseph Steel <62897806+SteelMasterJ@users.noreply.github.com>
2021-07-13 18:07:47 -0400

    components added and functioning

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <p>Hey!</p>
-  <greeting></greeting>
+  <h3>Hey!</h3>
+
+  <greeting :age="age"></greeting>
+  <user :age="age" @age-change="updateAge"></user>
 </template>
 
 <script>
 import Greeting from './components/Greeting.vue';
+import User from './components/User.vue';
 
  export default {
-   name: "App",
-   components: {
-     Greeting,
-   }
+    name: "App",
+    components: {
+      Greeting,
+      User,
+    },
+    data() {
+      return {
+        age: 20,
+      }
+    },
+    methods: {
+      updateAge(num) {
+        this.age += num
+      }
+    }
  };
 </script>
