8cce84f681d802bcdaaab8f4508fca1f98f92b86
Joseph Steel <62897806+SteelMasterJ@users.noreply.github.com>
2021-07-14 11:42:36 -0400

    Props & Events

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,5 +1,6 @@
 <template>
     <button type="button" @click="onClickAge">Update Age</button>
+    <button type="button" @click="ageChangeFn(3)">Update Age CB</button>
     <p>The user is {{age}} years old</p>
     <p>{{ageDoubled}}</p>
 </template>
@@ -7,7 +8,17 @@
 <script>
 export default {
     name: "User",
-    props: ["age"],
+    props: {
+        age: {
+            type: Number,
+            // required: true,
+            // default: 20,
+            validator(value) {
+                return value < 130
+            }
+        },
+        ageChangeFn: Function,
+    },
     emits: ["age-change"],
     computed: {
         ageDoubled() {
